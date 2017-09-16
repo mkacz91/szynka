3fd291b8195804d61ee3e31295e8a6a4ab2480b7
Marcin Kaczmarek <mkacz91@gmail.com>
2017-09-16 17:24:16 +0200

    Render normal and rotated shape

--- src/shaders/fill_vx.glsl
+++ src/shaders/fill_vx.glsl
@@ -1,8 +1,14 @@
-uniform mat3 transform;
+uniform vec4 transform;
 
 attribute vec2 position;
 
+vec2 apply_transform(vec4 transform, vec2 position) {
+    // transform = [cos(phi), sin(phi), dx, dy]
+    vec4 p = transform.xyyx * position.xxyy * vec4(1.0, 1.0, -1.0, 1.0);
+    return p.xy + p.zw + transform.zw;
+}
+
 void main ()
 {
-    gl_Position = vec4((transform * vec3(position, 1.0)).xy, 0.0, 1.0);
+    gl_Position = vec4(apply_transform(transform, position), 0.0, 1.0);
 }
--- src/szynka.cpp
+++ src/szynka.cpp
@@ -34,9 +34,9 @@ int main(int argc, char** argv)
 
     Assets::add_config("szynka_assets.config");
 
-    int framebuffer_width = 512, framebuffer_height = 512;
     GLuint framebuffer;
     glGenFramebuffersEXT(1, &framebuffer);
+    int framebuffer_size = 256;
     glBindFramebufferEXT(GL_FRAMEBUFFER, framebuffer);
 
     GLuint renderbuffers[2];
@@ -45,7 +45,7 @@ int main(int argc, char** argv)
     GLuint color_attachment = renderbuffers[0];
     glBindRenderbufferEXT(GL_RENDERBUFFER, color_attachment);
     glRenderbufferStorageMultisampleEXT(
-        GL_RENDERBUFFER, max_samples - 1, GL_RGBA8, framebuffer_width, framebuffer_height);
+        GL_RENDERBUFFER, max_samples - 1, GL_RGBA8, framebuffer_size, framebuffer_size);
     glFramebufferRenderbufferEXT(
         GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, color_attachment);
 
@@ -53,20 +53,19 @@ int main(int argc, char** argv)
     glBindRenderbufferEXT(GL_RENDERBUFFER, stencil_attachment);
     glRenderbufferStorageMultisampleEXT(
         GL_RENDERBUFFER, max_samples - 1, GL_STENCIL_INDEX1_EXT,
-        framebuffer_width, framebuffer_height);
+        framebuffer_size, framebuffer_size);
     glFramebufferRenderbufferEXT(
         GL_FRAMEBUFFER, GL_STENCIL_ATTACHMENT, GL_RENDERBUFFER, stencil_attachment);
 
-    glViewport(0, 0, framebuffer_width, framebuffer_height);
+    glViewport(0, 0, framebuffer_size, framebuffer_size);
 
     std::vector<vec2f> local_vertices =
     {
-        { 0, 0 },
-        { 1, 0 },
-        { -0.5f, 0.5f },
-        { 1, 1 },
-        { 0, 1 },
-        { 0, 0 }
+        { 0.10f, 0.10f },
+        { 0.90f, 0.10f },
+        { 0.10f, 0.90f },
+        { 0.90f, 0.70f },
+        { 0.10f, 0.10f }
     };
     int_range vertex_range(0, local_vertices.size());
     GLuint vertices = 0;
@@ -77,40 +76,54 @@ int main(int argc, char** argv)
     GLint fill_transform_uniform = gl::get_uniform_location(fill_program, "transform");
     GLint fill_position_attrib = gl::get_attrib_location(fill_program, "position");
 
-    glClearColor(1.0, 1.0, 1.0, 1.0);
+    glClearColor(0.0, 0.0, 0.0, 0.0);
     glClear(GL_COLOR_BUFFER_BIT);
     glClearStencil(0);
     glClear(GL_STENCIL_BUFFER_BIT);
 
     glUseProgram(fill_program);
-    szynka::glUniformMatrix3fv(fill_transform_uniform, eye3f().scale(0.5f));
     glEnableVertexAttribArray(fill_position_attrib);
     glVertexAttribPointer(fill_position_attrib, 2, GL_FLOAT, GL_FALSE, 0, nullptr);
-
     glEnable(GL_STENCIL_TEST);
-    glStencilFunc(GL_ALWAYS, 0x00, 0x01);
-    glStencilOp(GL_INVERT, GL_INVERT, GL_INVERT);
-    glColorMask(false, false, false, false);
 
+    glUniform4f(fill_transform_uniform, 1.0f, 0.0f, -1.0f, -1.0f);
+    glStencilMask(0x01);
+
+    glStencilOp(GL_INVERT, GL_INVERT, GL_INVERT);
+    glStencilFunc(GL_ALWAYS, 0x00, 0x01);
+    glColorMask(false, false, false, false);
     szynka::glDrawArrays(GL_TRIANGLE_FAN, vertex_range);
 
-    glStencilFunc(GL_EQUAL, 0x01, 0x01);
     glStencilOp(GL_KEEP, GL_KEEP, GL_KEEP);
-    glColorMask(true, true, true, true);
+    glStencilFunc(GL_EQUAL, 0x01, 0x01);
+    glColorMask(true, false, false, false);
+    szynka::glDrawArrays(GL_TRIANGLE_FAN, vertex_range);
 
+    glUniform4f(fill_transform_uniform, 1.0f, 1.0f,
+        (float)(-3) / (float)(2 * framebuffer_size),
+        (float)(1 - 2 * framebuffer_size) / (2 * framebuffer_size));
+    glStencilMask(0x02);
+    glStencilOp(GL_INVERT, GL_INVERT, GL_INVERT);
+    glStencilFunc(GL_ALWAYS, 0x00, 0x02);
+    glColorMask(false, false, false, false);
+    szynka::glDrawArrays(GL_TRIANGLE_FAN, vertex_range);
+
+    glStencilOp(GL_KEEP, GL_KEEP, GL_KEEP);
+    glStencilFunc(GL_EQUAL, 0x02, 0x02);
+    glColorMask(false, true, false, false);
     szynka::glDrawArrays(GL_TRIANGLE_FAN, vertex_range);
 
     glDisableVertexAttribArray(fill_position_attrib);
-
     glDisable(GL_STENCIL_TEST);
+    glColorMask(true, true, true, true);
 
     glBindFramebufferEXT(GL_DRAW_FRAMEBUFFER, 0);
     glViewport(0, 0, window_width, window_height);
     glClearColor(0.8f, 0.3f, 0.6f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
     glBlitFramebufferEXT(
-        0, 0, framebuffer_width, framebuffer_height,
-        10, 10, 10 + framebuffer_width, 10 + framebuffer_height,
+        0, 0, framebuffer_size, framebuffer_size,
+        10, 10, 10 + framebuffer_size, 10 + framebuffer_size,
         GL_COLOR_BUFFER_BIT, GL_NEAREST);
 
     glfwSwapBuffers(window);
